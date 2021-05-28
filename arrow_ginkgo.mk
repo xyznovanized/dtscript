019859752b9e7dab5d3b5baddb586ec2f62c17bb
Bill Yi <byi@google.com>
2021-05-27 21:39:42 -0700

    ginkgo: Bypass build time check for users-libs
    * set PRODUCT_BROKEN_VERIFY_USES_LIBRARIES to true to bypss the build
    time check on users-libs to fix the build error.
    
    Bug: 189477034
    Test: pass the build
    Change-Id: I4343d46d4fe8fc1e24a3d7c9f4e88908bdc651bd

--- arrow_ginkgo.mk
+++ arrow_ginkgo.mk
@@ -28,6 +28,10 @@ TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_INCLUDE_PIXEL_CHARGER := true
 $(call inherit-product, vendor/arrow/config/common.mk)
 
+# b/189477034: Bypass build time check on uses_libs until vendor fixes all their apps
+PRODUCT_BROKEN_VERIFY_USES_LIBRARIES := true
+RELAX_USES_LIBRARY_CHECK := true
+
 # Device identifier
 PRODUCT_NAME := arrow_ginkgo
 PRODUCT_DEVICE := ginkgo
