b138ff76e18a12ec745fdd3455455d5aa3b99d0d
rasenss <73821326+rasenss@users.noreply.github.com>
2022-02-07 16:08:38 +0700

    ginkgo: use raven-user 12 SQ1D.220105.007 8030436 release-keys

--- aosp_ginkgo.mk
+++ aosp_ginkgo.mk
@@ -45,3 +45,14 @@ PRODUCT_MODEL := Redmi Note 8
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 DEVICE_MAINTAINER := Adithya (ghostrider_reborn)
+
+
+# Fingerprint
+BUILD_FINGERPRINT := google/raven/raven:12/SQ1D.220105.007/8030436:user/release-keys
+BUILD_DESCRIPTION := raven-user 12 SQ1D.220105.007 8030436 release-keys
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.fingerprint=$(BUILD_FINGERPRINT)
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="$(BUILD_DESCRIPTION)"
